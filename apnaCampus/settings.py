9c854da9c117d60527d0d93d1ceab9c63a109a3f
Akash Jatav <akjatav128@gmail.com>
2024-01-15 14:32:58 +0530

    second

--- apnaCampus/settings.py
+++ apnaCampus/settings.py
@@ -146,6 +146,6 @@ MESSAGE_TAGS={
 #SMTP configuration
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER ='akash5july2000@gmail.com'
-EMAIL_HOST_PASSWORD ='Iitkanpur123@'
+EMAIL_HOST_USER =''
+EMAIL_HOST_PASSWORD =''
 EMAIL_USE_TLS = True
