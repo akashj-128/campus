f1af69a566b0b7ccfa18083ec80c506873a129ba
Akash Jatav <akjatav128@gmail.com>
2024-01-16 17:49:34 +0530

    resetPassword

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,15 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('', views.dashboard, name='dashboard'),
+    path('forgotPassword/', views.forgotPassword, name='forgotPassword'),
+
     path('activate/uidb64/token/', views.activate, name='activate'),
+    path('reset/uidb64/token/', views.reset, name='reset'),
+    path('reset/<str:uidb64>/<str:token>/', views.reset, name='reset'),
     path('activate/<str:uidb64>/<str:token>/', views.activate, name='activate'),
+    path('resetPassword/', views.resetPassword, name='resetPassword'),
+
+
 ]
