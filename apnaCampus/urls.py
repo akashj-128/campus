3793ec2da85300b4b1b4199488ac9c312d63ed9e
Akash Jatav <akjatav128@gmail.com>
2024-01-18 03:02:17 +0530

    billing address

--- apnaCampus/urls.py
+++ apnaCampus/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 from django.conf.urls.static import static
+from django.urls import path, include
 from django.conf import settings
 
 urlpatterns =[
@@ -9,6 +10,7 @@ urlpatterns =[
     path('', views.home, name ='home'),
     path('store/', include('store.urls')),
     path('cart/', include('carts.urls')),
-     path('accounts/', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('orders/', include('orders.urls')),
 
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
